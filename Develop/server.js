0489bb5d03dcef44c8925c1a812d7d25747ff5c5
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-26 17:29:55 -0500

    added instance of sequelize store

--- Develop/server.js
+++ Develop/server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/config");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const sess = {
