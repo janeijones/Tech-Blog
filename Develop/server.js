131698c3b743b3a747484992a989b03dc3219d6d
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-26 17:00:29 -0500

    Sess added to server.js '

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const sequelize = require("./config/config");
+
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess)); 
