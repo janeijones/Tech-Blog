ec9ae7fe420e02e56aff56d516cc260b5c47ac75
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-26 17:06:56 -0500

    hbs and listener added

--- Develop/server.js
+++ Develop/server.js
@@ -12,3 +12,19 @@ const sess = {
 };
 
 app.use(session(sess)); 
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false })
+})
