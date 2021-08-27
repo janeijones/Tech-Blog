3499e3df67b77e983cc5e9ce80660aae30254a8e
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-26 23:51:47 -0500

    Added logic for logout.js file

---
+++ Develop/public/js/logout.js
@@ -0,0 +1,15 @@
+//user selects logout
+const logoutHandler = async (req, res) => {
+    const response = await fetch('/api/user/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+  
+document.querySelector('#logout').addEventListener('click', logoutHandler);
