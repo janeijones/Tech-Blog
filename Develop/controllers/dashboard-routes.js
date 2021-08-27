2bfacafca2b34475ff64e3929b4b0ea4b4227393
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-26 22:53:50 -0500

    Added routed created by user, allowing user to edit, and create new post

--- Develop/controllers/dashboard-routes.js
+++ Develop/controllers/dashboard-routes.js
@@ -1,4 +1,45 @@
 const router = require('express').Router();
 const { Post } = require('../models/');
 
-const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth'); //middleware used to ensure session is logged in
+
+
+router.get('/', withAuth, async (req, res) => { //get all the posts created by user
+    try {
+        const postData = await Post.findAll({
+            where: {
+                userId: req.session.userId,
+            }
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }))
+
+        res.render('all-posts-admin', { layout: 'dashboard', posts, loggedIn: true });
+    } catch (err) {
+        res.redirect('/')
+    }
+});
+
+
+router.get('/new', withAuth, async (req,res) => { //user to create a post
+    res.render('new-post', { layout: 'dashboard', loggedIn: true });
+});
+
+
+router.get('/edit-post/:id', withAuth, async (req, res) => { //route to allow user to edit post by id
+    try {
+        const postData = await Post.findByPk(req.params.id)
+
+        if (postData) {
+            const post = postData.get({ plain: true });
+
+            res.render('edit-post', { layout: 'dashboard', post, loggedIn: true });
+        } else {
+            res.status(404).end();
+        }
+    }   catch {
+        res.redirect('login')
+    }
+});
+
+module.exports = router;
