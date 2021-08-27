abc7793dfb1e2d41a8706ae12d2a321f0215ea53
Janei <18485105+janeijones@users.noreply.github.com>
2021-08-26 21:21:16 -0500

    IUpdated routes. post to get individual comment

---
+++ Develop/controllers/home-routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models/');
+
+router.get('/', async (req, res) => { //gets all posts for homepage
+    try {
+        const postData = await Post.findAll({
+            include: [User],
+        });
+        const posts = postData.map((post) => post.get({ plain : true }));
+
+        res.render('all-posts', { posts });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/post/:id', async (req, res) => { //gets one posted comment 
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: [User],
+                },
+            ],
+        });
+
+        if (postData) { // if post is found, render individual post
+            const post = postData.get({ plain: true });
+
+            res.render('single-post', { post });
+            
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500). json(err);
+    }
+});
